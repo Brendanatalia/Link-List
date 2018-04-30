363fe81315c6a7fe718eb95f70a7e5940d4b7cbd
NaomyTiaraDewi <38529620+NaomyTiaraDewi@users.noreply.github.com>
2018-04-30 10:28:06 +0700

    menambah tail di linked link list

--- linked list.cpp
+++ linked list.cpp
@@ -1,174 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct node 
+struct node{
+int data;
+node *next;
+};
+
+class linkedlist
 {
-  int data;
-  node *next;
-}; 
-node *head;
-void init()
+private:
+node *head, *tail;
+public:
+linkedlist()
 {
-    head = NULL;
+head=NULL;
+tail=NULL;
 }
-
-bool isEmpty()
-{
-    if (head == NULL)
-        return 1;
-    return 0;
-}
-
-void insertDepan(int databaru);
-void insertBelakang(int databaru);
-void deleteDepan();
-void deleteBelakang();
-void insertNode();
-void deleteNode();
-
-
-int main(){
-	init();
-	int data,n;
-	cin>>n;
-	for(int i=0;i<n;i++){
-		cin>>data;
-		insertDepan(data);
-	}
-	
-
-void insertDepan(int databaru){
-	node *baru;
-	baru= new node();
-	baru->data=databaru;
-	baru->next=baru;
-	if(isEmpty()==1){
-		head=baru;
-		head->next=head;	
-	}
-	else{
-		node *bantu;
-		bantu=head;
-		do{
-			bantu=bantu->next;
-		}
-		while(bantu->next!=head);
-		baru->next=head;
-		head=baru;
-		bantu->next=head;
-	}
-	  if (head!=NULL){
-        node *bantu;
-        bantu = head;
-        do{
-            cout<<bantu->data;
-            bantu=bantu->next;
-        }while (bantu!=head);
-    } else {
-        cout<<"List Kosong";
-    }
-}
-
-void insertBelakang(int databaru){
-	node *baru, *bantu;
-	baru= new node();
-	baru->data=databaru;
-	baru->next=NULL;
-	if(isEmpty()==1){
-		head=baru;
-		head->next=NULL;	
-	}
-	else{
-		bantu=head;
-		do{
-			bantu=bantu->next;
-		}
-		while(bantu->next!=head);
-		baru->next=NULL;
-		bantu->next=head;
-	}
-	if (head!=NULL){
-        bantu = head;
-        do{
-            cout<<bantu->data;
-            bantu=bantu->next;
-        }while (bantu!=head);
-    } else {
-        cout<<"List Kosong";
-    }
-
-}
-	void hapusDepan (){
-	TNode *hapus,*bantu;
-	if (isEmpty()==0){
-		int d;
-		hapus = head;
-		d = head->data;
-		if(head->next != head){
-			bantu = head;
-			while(bantu->next!=head){
-				bantu=bantu->next;
-			}
-			head = head->next;
-			delete hapus;
-			bantu->next = head;
-		}else{
-			head=NULL;
-		}
-		cout<<"terhapus";
-	} else cout<<"Masih kosong";
-}
-		void hapusTengah(){
-		TNode *bantu,*hapus;
-		int d;
-		if (isEmpty()==0){
-			if(head!=tail){
-				hapus = bantu;
-				d = hapus->data;
-				head = head->next;
-				delete hapus;
-			} else {
-				d = tail->data;
-				head=tail=NULL;
-			}
-		cout<<("%d terhapus\n",d);
-		} 
-		else cout<<("Masih kosong\n");
-	}
-
-void hapusBelakang(){
-	TNode *bantu,*hapus;
-	int d;
-	if (isEmpty()==0){
-		bantu = head;
-		if(head!=tail){
-			while(bantu->next!=tail){
-				bantu = bantu->next;
-				}
-			hapus = tail;
-			tail=bantu;
-			d = hapus->data;
-			delete hapus;
-			tail->next = NULL;
-		}else {
-			d = tail->data;
-			head=tail=NULL;
-			}
-		cout<<d<<" terhapus\n";
-	} else cout<<"Masih kosong\n";
-}
-
-void clear(){
-	TNode *bantu,*hapus;
-	bantu = head;
-	while(bantu!=NULL)
-	{
-		hapus = bantu;
-		bantu = bantu->next;
-		delete hapus;
-	}
-	head = NULL;
-	tail = NULL;
-}
-
-	
+};//kita pake head and tail aja yaa guys
