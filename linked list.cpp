0d9203983127efa871070f4df3deffbcb9da756e
oktaviana1007 <38560190+oktaviana1007@users.noreply.github.com>
2018-04-30 14:37:43 +0700

    Update linked list.cpp

--- linked list.cpp
+++ linked list.cpp
@@ -16,4 +16,38 @@ linkedlist()
 head=NULL;
 tail=NULL;
 }
-};//kita pake head and tail aja yaa guys
+};//kita pake head and tail aja yaa guys, oke mi
+
+ void buatnode(int value){ //ini tambah data di belakang gaesss
+    node *baru =new node;
+    baru->data=value;
+    baru->next=NULL;
+    if(head==NULL){
+	head=baru;
+	tail=baru;
+    baru=NULL;      
+    }
+    else
+    {	
+    tail->next=baru;
+    tail=baru;
+}
+}
+
+void tampilan(){
+    node *baru=new node;
+    baru=head;
+    while(baru!=NULL)
+    {
+      cout<<baru->data<<"  ";
+      baru=baru->next;
+    } 
+}
+  
+    void tambahdepan(int nilai)
+  {
+    node *baru=new node;
+    baru->data=nilai;
+    baru->next=head;
+    head=baru;
+  }
