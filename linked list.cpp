3f2251e8f53f4b24a6707599fb006cf04443bd22
cahyaniramadhani <38560233+cahyaniramadhani@users.noreply.github.com>
2018-04-30 15:32:24 +0700

    Update linked list.cpp

--- linked list.cpp
+++ linked list.cpp
@@ -16,7 +16,7 @@ linkedlist()
 head=NULL;
 tail=NULL;
 }
-};//kita pake head and tail aja yaa guys, oke mi
+};//kita pake head and tail aja yaa guys, oke mi, okelah mi
 
  void buatnode(int value){ //ini tambah data di belakang gaesss
     node *baru =new node;
@@ -51,3 +51,27 @@ void tampilan(){
     baru->next=head;
     head=baru;
   }
+   void tambahtengah(int letak, int nilai)
+  {
+    node *sebelum=new node;
+    node *sesudah=new node;
+    node *baru=new node;
+    sesudah=head;
+    for(int i=1;i<letak;i++)
+    {
+      sebelum=sesudah;
+      sesudah=sesudah->next;
+    }
+    baru->data=nilai;
+    sebelum->next=baru;	
+    baru->next=sesudah;
+  }
+  
+  void hapusdepan()
+  {
+    node *baru=new node;
+    baru=head;
+    head=head->next;
+    delete baru;
+  }
+
